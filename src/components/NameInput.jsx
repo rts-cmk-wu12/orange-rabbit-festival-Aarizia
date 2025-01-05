33e9d1d3170e4cce6f248550df3942a7da75615c
Aarizia <emma_489@hotmail.com>
2025-01-05 11:40:03 +0100

    add files

---
+++ src/components/NameInput.jsx
@@ -0,0 +1,58 @@
+import { useRef } from "react"
+import { useState } from "react"
+import '../style/components/_error-message.scss'
+
+export default function NameInput( props ) {
+
+    const [error, setError] = useState(false)
+    const [errorLevel, setErrorLevel] = useState(-1)
+    const inputElement = useRef(null)
+
+    const style = [
+        { border: '2px solid #000000' },
+        { border: '2px solid #26a706' },
+        { border: '2px solid #d80a0a' }
+    ]
+    
+    function changeHandler() {
+        
+        const value = inputElement.current.value
+        const regEx = /^[^\d ]{2,}( [^\d ]{2,})+$/
+
+        if (!value.match(regEx)) {
+            setError('Name is not valid')
+            setErrorLevel(2)
+        } else if (value.length > 50) {
+            setError('Name is not valid')
+            setErrorLevel(2)
+        } else {
+            setError(false)
+            setErrorLevel(1)
+        }
+    }
+
+    function focusHandler() {
+
+        if (errorLevel !== 1) {
+            setErrorLevel(0)
+        }
+    }
+
+    function blurHandler() {
+
+        const value = inputElement.current.value
+
+        if (error === false && value != '') {
+            setErrorLevel(1)
+        } else {
+            setErrorLevel(-1)
+        }
+    }
+
+    return (
+        <>
+            <input style={style[errorLevel]} ref={inputElement} onChange={changeHandler} onFocus={focusHandler} onBlur={blurHandler} {...props} />
+            {error && <p className="error-message">{error}</p>}
+        </>
+    )
+}
---
+++ src/pages/confirmation.jsx
@@ -0,0 +1,14 @@
+export default function Confirmation() {
+
+    return (
+        <div className="confirmation">
+            <section>
+                <img src="../../src/assets/icons/done-icon.png" alt="done icon" />
+                <p>Confirmation</p>
+                <h1>Signup complete</h1>
+                <p>We look forward to seeing you at the event</p>
+                <button>Order more tickets</button>
+            </section>
+        </div>
+    )
+}
